bba1ee159aaa6823c67ca8f3ca8ded81f12528d2
Julian Safadi <julianmatiassafadi@hotmail.com>
2022-12-06 15:19:36 -0300

    update changes

---
+++ src/navigation.js
@@ -0,0 +1,136 @@
+searchFormBtn.addEventListener('click', () => {
+    location.hash = '#search=' + searchFormInput.value
+})
+
+trendingBtn.addEventListener('click', () => { location.hash = '#trends' })
+
+arrowBtn.addEventListener('click', () => {
+    location.hash = window.history.back();
+})
+
+window.addEventListener('DOMContentLoaded', navigator, false)
+window.addEventListener('hashchange', navigator, false)
+
+function navigator() {
+    console.log({location})
+
+    if(location.hash.startsWith('#trends')) {
+        trendsPage()
+    } else if (location.hash.startsWith('#search=')) {
+        searchPage()
+    } else if (location.hash.startsWith('#movie=')) {
+        moviesDetailsPage()
+    } else if (location.hash.startsWith('#category=')) {
+        categoriesPage()
+    } else {
+        homePage()
+    }
+    window.scrollTo(0, 0);
+}
+
+function homePage() {
+    console.log('home')
+
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ''
+    arrowBtn.classList.add('inactive')
+    arrowBtn.classList.remove('header-arrow--white')
+    headerTitle.classList.remove('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.remove('inactive')
+
+    trendingPreviewSection.classList.remove('inactive')
+    categoriesPreviewSection.classList.remove('inactive')
+    genericSection.classList.add('inactive')
+    movieDetailSection.classList.add('inactive')
+
+    getTrendingMoviesPreview()
+    getCategoriesPreview()
+}
+
+function categoriesPage() {
+    console.log('categories!!')
+
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ''
+    arrowBtn.classList.remove('inactive')
+    arrowBtn.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.remove('inactive')
+    searchForm.classList.add('inactive')
+
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
+
+
+    const [_, categoryData] = location.hash.split('=') // ['#category', 'id-name']
+    const [categoryId, categoryName] = categoryData.split('-')
+
+    headerCategoryTitle.innerHTML = categoryName
+
+    getMoviesByCategory(categoryId)
+    console.log(categoryId)
+}
+
+function moviesDetailsPage() {
+    console.log('movie!')
+
+    headerSection.classList.add('header-container--long')
+    // headerSection.style.background = ''
+    arrowBtn.classList.remove('inactive')
+    arrowBtn.classList.add('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.add('inactive')
+
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericSection.classList.add('inactive')
+    movieDetailSection.classList.remove('inactive')
+
+    const [_, movieId] = location.hash.split('=') // ['#movie', id]
+    getMovieById(movieId)
+}
+
+function searchPage() {
+    console.log('search!')
+
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ''
+    arrowBtn.classList.remove('inactive')
+    arrowBtn.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.remove('inactive')
+
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
+
+    const [_, query] = location.hash.split('=') // ['#search', 'searched']
+    getMoviesBySearch(query)
+}
+
+function trendsPage() {
+    console.log('trends!')
+
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ''
+    arrowBtn.classList.remove('inactive')
+    arrowBtn.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.remove('inactive')
+    searchForm.classList.remove('inactive')
+
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
+
+    headerCategoryTitle.innerHTML = 'Tendencias'
+
+    getTrendingMovies()
+}
